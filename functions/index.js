9510a8e4d09b910a98cab193b89d662ed1f49fb5
Maya Mubayi <mayamubayi@Mayas-MacBook-Pro.local>
2019-11-07 14:48:23 -0500

    enabling user sync with database and added customisation upon user log-in

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,16 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Add a user to Firestore database:
+exports.setupUsers = functions.auth.user().onCreate(async (user) => {
+  var fullName = user.displayName || 'Anonymous';
+  await admin.firestore().collection('users').doc(user.uid).set({
+    name: fullName,
+    id: user.uid,
+  });
+});
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
